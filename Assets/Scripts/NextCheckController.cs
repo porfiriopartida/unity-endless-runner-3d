0060b89c61f90d9165a5dffa2868608d7ffd046a
Porfirio Partida <ppartida@skytouchtechnology.com>
2016-07-26 15:52:14 -0700

    Initial Phase of Endless Sphere Runner.

---
+++ Assets/Scripts/NextCheckController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextCheckController : MonoBehaviour {
+    public static string LAST_PIECE = "FRONT";
+    public static GameObject toBeDestroyed = null;
+    public GameObject validPrefab;
+    public static GameObject LAST_PIECE_OBJECT;
+    public static int piecesCounter = 0;
+    private GameObject piecesWrapper;
+
+    void Start(){
+        if (LAST_PIECE_OBJECT == null) {
+            LAST_PIECE_OBJECT = GameObject.Find ("Last");
+        }
+        piecesWrapper = GameObject.Find ("Pieces");
+    }
+
+    void OnTriggerEnter(Collider other) {
+        GameObject gameObject = other.gameObject;
+        if(gameObject != null && gameObject.CompareTag("Player")){
+            addNewPiece ();
+
+            destroyFirstPiece ();
+        }
+    }
+
+    void addNewPiece (){
+        string sValue = getRandomPiece ();
+        LAST_PIECE = sValue;
+
+        //TODO: Change depending on last piece value.
+        GameObject newFloor = Instantiate (validPrefab);
+
+        float offset = 90f;
+        //Reubicate the new piece in front of the last piece.
+        newFloor.transform.position = new Vector3(LAST_PIECE_OBJECT.transform.position.x, LAST_PIECE_OBJECT.transform.position.y, LAST_PIECE_OBJECT.transform.position.z + offset);
+        newFloor.name = "Piece" +(++piecesCounter);
+        newFloor.transform.parent = piecesWrapper.transform;
+        //LAST_PIECE_OBJECT.name = "NotLast";
+        //New last piece added.
+        LAST_PIECE_OBJECT = newFloor;
+    }
+    void destroyFirstPiece(){
+        if (toBeDestroyed != null) {
+            Destroy (toBeDestroyed);
+            toBeDestroyed = null;
+        }
+        toBeDestroyed = gameObject.transform.parent.gameObject;
+        Destroy (gameObject); //Destroying the collider.
+    }
+    string getRandomPiece(){
+        int value = Mathf.RoundToInt(Random.value * 2);
+        string sValue;
+        switch(value){
+            case 1:
+                sValue = "LEFT";
+                break;
+            case 2:
+                sValue = "RIGHT";
+                break;
+            case 0:
+            default:
+                sValue = "FRONT";
+                break;
+        }
+        return sValue;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float xSpeed, movSpeed;
+    Rigidbody rb;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody> ();
+        rb.velocity =  new Vector3 (0f, 0.0f, movSpeed);
+    }
+    void FixedUpdate(){
+        rb.velocity =  new Vector3 (0f, rb.velocity.y, movSpeed);
+    }
+    int currentPosition = 0;
+    // Update is called once per frame
+    void Update () {
+        float horizontalAxis = Input.GetAxis ("Horizontal");
+        bool buttonHorizontal = Input.GetButtonDown ("Horizontal");
+        //float moveVertical = Input.GetAxis ("Vertical");
+
+        if (buttonHorizontal) {
+            if (horizontalAxis < 0 && currentPosition >= 0) {
+                currentPosition -= 10;
+            } else if (horizontalAxis > 0 && currentPosition <= 0) {
+                currentPosition += 10;
+            }
+            transform.position = new Vector3 (currentPosition, transform.position.y, transform.position.z);
+        }
+    }
+}
