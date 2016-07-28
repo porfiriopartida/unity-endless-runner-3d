681c542c102c895812c1447672f628e99556d603
Porfirio Partida - home <porfirio.partidai@gmail.com>
2016-07-27 22:42:50 -0700

    Control editor, lizzard back, rebuilding prefabs.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,69 +2,12 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	public static float movSpeed = 40f;
+    public float m_movSpeed;
     Rigidbody rb;
-    public static Vector3 velocity = new Vector3 (0f, 0.0f, movSpeed);
-    public static void rotateRight(){
-        rotate (true);
-    }
-    public static void rotateLeft(){
-        rotate (false);
-    }
-    private static void rotate(bool right){
-        GameObject player = GameObject.Find ("Player");
-        GameObject camera = GameObject.Find ("Main Camera");
-        FollowCameraController cameraController = camera.GetComponent<FollowCameraController> ();
-
-        //velocity = new Vector3 (movSpeed, 0.0f, 0f);
-        if (right) {
-            if(velocity.x > 0){
-                //Traveling through +X. Turning to the Right. -Z
-                velocity = new Vector3 (0f, 0f, -movSpeed);
-            } else if (velocity.z > 0){
-                //Traveling through +Z. Turning to the Right. +X
-                velocity = new Vector3 (+movSpeed, 0f, 0f);
-            } else if(velocity.x < 0){
-                //Traveling through -X. Turning to the Right. +Z
-                velocity = new Vector3 (0f, 0f, movSpeed);
-            }else if(velocity.z < 0){
-                //Traveling through -X. Turning to the Right. +Z
-                velocity = new Vector3 (-movSpeed, 0f, 0f);
-            }
-            //TODO: Travel through Y ?
-        } else {
-            if(velocity.x > 0){
-                //Traveling through +X. Turning to the LEFT. -Z
-                velocity = new Vector3 (0f, 0f, +movSpeed);
-            } else if (velocity.z > 0){
-                //Traveling through +Z. Turning to the LEFT. +X
-                velocity = new Vector3 (-movSpeed, 0f, 0f);
-            } else if(velocity.x < 0){
-                //Traveling through -X. Turning to the LEFT. +Z
-                velocity = new Vector3 (0f, 0f, -movSpeed);
-            }else if(velocity.z < 0){
-                //Traveling through -X. Turning to the LEFT. +Z
-                velocity = new Vector3 (+movSpeed, 0f, 0f);
-            }
-            //TODO: Travel through Y ?
-        }
-
-        float angle = (right ? 90 : -90);
-
-        //player.transform.RotateAround(player.transform.position, new Vector3(0, 1, 0), angle);
-        cameraController.buildOffset(velocity);
-        player.transform.Rotate (new Vector3(0, angle, 0));
-    }
-    // Use this for initialization
-    void Start () {
-        rb = GetComponent<Rigidbody> ();
-		rb.velocity =  velocity;
-	}
-	void FixedUpdate(){
-		rb.velocity =  velocity;
-    }
+    private Vector3 m_velocity;
+    
     float currentPosition = 0;
-
+    
     float hAccValue = 0.3f; //if windows = 0.
     enum Movements { LEFT, RIGHT, NONE};
     private Movements lastMovement = Movements.NONE;
@@ -72,40 +15,106 @@ public class PlayerController : MonoBehaviour {
     float delayBetweenMovements;
     float timeBetweenMovements = 0.5f;
     float timeBetweenRepeatedMovements = 1f;
+
+    public void RotateRight(){
+        rotate (true);
+    }
+    public void RotateLeft(){
+        rotate (false);
+    }
+    private void rotate(bool right){
+        GameObject player = GameObject.Find ("Player");
+        GameObject camera = GameObject.Find ("Main Camera");
+        FollowCameraController cameraController = camera.GetComponent<FollowCameraController> ();
+
+        //velocity = new Vector3 (movSpeed, 0.0f, 0f);
+        if (right) {
+            if(m_velocity.x > 0){
+                //Traveling through +X. Turning to the Right. -Z
+                m_velocity = new Vector3 (0f, 0f, -m_movSpeed);
+            } else if (m_velocity.z > 0){
+                //Traveling through +Z. Turning to the Right. +X
+                m_velocity = new Vector3 (+m_movSpeed, 0f, 0f);
+            } else if(m_velocity.x < 0){
+                //Traveling through -X. Turning to the Right. +Z
+                m_velocity = new Vector3 (0f, 0f, m_movSpeed);
+            }else if(m_velocity.z < 0){
+                //Traveling through -X. Turning to the Right. +Z
+                m_velocity = new Vector3 (-m_movSpeed, 0f, 0f);
+            }
+            //TODO: Travel through Y ?
+        } else {
+            if(m_velocity.x > 0){
+                //Traveling through +X. Turning to the LEFT. -Z
+                m_velocity = new Vector3 (0f, 0f, +m_movSpeed);
+            } else if (m_velocity.z > 0){
+                //Traveling through +Z. Turning to the LEFT. +X
+                m_velocity = new Vector3 (-m_movSpeed, 0f, 0f);
+            } else if(m_velocity.x < 0){
+                //Traveling through -X. Turning to the LEFT. +Z
+                m_velocity = new Vector3 (0f, 0f, -m_movSpeed);
+            }else if(m_velocity.z < 0){
+                //Traveling through -X. Turning to the LEFT. +Z
+                m_velocity = new Vector3 (+m_movSpeed, 0f, 0f);
+            }
+            //TODO: Travel through Y ?
+        }
+
+        float angle = (right ? 90 : -90);
+        //player.transform.RotateAround(player.transform.position, new Vector3(0, 1, 0), angle);
+        cameraController.buildOffset(m_velocity);
+        player.transform.Rotate (new Vector3(0, angle, 0));
+        rb.velocity =  new Vector3(0,0,0);
+        rb.AddForce(m_velocity);
+    }
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody> ();
+        m_velocity = new Vector3 (0f, 0.0f, m_movSpeed);
+        rb.AddForce(m_velocity);
+    }
+//    void FixedUpdate(){
+//        rb.velocity =  velocity;
+//    }
     // Update is called once per frame
     void Update () {
-		if(Time.time > delayBetweenMovements){
-			//if WINDOWS
-//			float horizontalAxis = Input.GetAxis ("Horizontal");
-//			bool buttonHorizontal = Input.GetButtonDown ("Horizontal");
-			//If Android-iPhone
-			float horizontalAxis = Input.acceleration.x;
-			bool buttonHorizontal = Mathf.Abs (horizontalAxis) > hAccValue;
+        if(Time.time > delayBetweenMovements){
+            //if WINDOWS
+            //If Android-iPhone
+			#if UNITY_EDITOR
+            float horizontalAxis = Input.GetAxis ("Horizontal");
+            bool buttonHorizontal = Input.GetButtonDown ("Horizontal");
+			hAccValue = 0;
+			#else
+            float horizontalAxis = Input.acceleration.x;
+            bool buttonHorizontal = Mathf.Abs (horizontalAxis) > hAccValue;
+			#endif
 			if (buttonHorizontal) {
-				print ("- Horizontal Axis " + horizontalAxis);
 				Movements actualMovement = Movements.NONE;
 				float diff = 0;
 				if (horizontalAxis < hAccValue && currentPosition >= 0) {
-                    //To the left
-                    actualMovement = Movements.LEFT;
-                    diff = -10f;
-                } else if (horizontalAxis > hAccValue && currentPosition <= 0) {
-                    //To the right
-                    actualMovement = Movements.RIGHT;
-                    diff = +10f;
-                }
-                //Using delay
-                if(actualMovement != lastMovement || Time.time >= delayRepeatedMovements && actualMovement == lastMovement){
-                    currentPosition += diff;
-					transform.Translate(currentPosition * Time.smoothDeltaTime, 0, 0);
-                    transform.position = new Vector3 (currentPosition, transform.position.y, transform.position.z);
-                    delayRepeatedMovements = Time.time + timeBetweenRepeatedMovements;
-                    delayBetweenMovements = Time.time + timeBetweenMovements;
-                    lastMovement = actualMovement;
-                }
-            } else {
-                delayRepeatedMovements = 0;
-            }
+					//To the left
+					actualMovement = Movements.LEFT;
+					diff = -10f;
+				} else if (horizontalAxis > hAccValue && currentPosition <= 0) {
+					//To the right
+					actualMovement = Movements.RIGHT;
+					diff = +10f;
+				}
+				//Using delay
+				if(actualMovement != lastMovement || Time.time >= delayRepeatedMovements && actualMovement == lastMovement){
+					currentPosition += diff;
+					//					transform.Translate(currentPosition * Time.smoothDeltaTime, 0, 0);
+//					transform.Translate(-Vector3.right * diff * Time.deltaTime);
+//					transform.position = new Vector3 (currentPosition, transform.position.y, transform.position.z);
+					transform.position = transform.position + Vector3.right * diff;
+					delayRepeatedMovements = Time.time + timeBetweenRepeatedMovements;
+					delayBetweenMovements = Time.time + timeBetweenMovements;
+					lastMovement = actualMovement;
+				}
+			} else {
+				delayRepeatedMovements = 0;
+			}
         }
     }
 }
--- Assets/TurnController.cs
+++ Assets/TurnController.cs
@@ -4,18 +4,20 @@ using System.Collections;
 public class TurnController : MonoBehaviour {
 	public bool right;
 	GameObject piecesWrapper;
+	PlayerController player;
 	// Use this for initialization
 	void Start () {
 		piecesWrapper = GameObject.Find ("Pieces");
+		player = GameObject.Find ("Player").GetComponent<PlayerController>();
 	}
 	void OnTriggerEnter(Collider collider){
 //		Transform transform = piecesWrapper.transform;
 		//		transform.RotateAround (collider.transform.position, new Vector3(0, 1, 0), -90);
 //		collider.transform.RotateAround (collider.transform.position, new Vector3(0, 1, 0), 90);
         if (right) {
-            PlayerController.rotateRight ();
+            player.RotateRight ();
         } else {
-            PlayerController.rotateLeft ();
+			player.RotateLeft ();
         }
         gameObject.SetActive (false);
 //		transform.position = transform.position + new Vector3(10, 0, 0);
